860f379d2a28ad41421655cf2a9cbd55f38df9e7
MD NAWED ALAM <nawedmr477@gmail.com>
2026-06-12 20:56:53 +0530

    Recursion

---
+++ Recursion/powers.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class powers {
+
+    static int power(int a, int b) {
+        if (b == 0)
+            return 1;
+
+        return a * power(a, b - 1);
+    }
+
+    public static void main(String[] args) {
+        power(4, 6);
+    }
+}
