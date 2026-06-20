c50aabe5623dcd4c95e572638356c53137c28767
MD NAWED ALAM <nawedmr477@gmail.com>
2026-06-20 18:36:11 +0530

    update

--- Browser History/WebVisit.java
+++ Browser History/WebVisit.java
@@ -1,44 +1,76 @@
-package Browser History;
+// package Browser History;
+// import java.util.Date;
+
+// public class WebVisit {
+// 	private String url;
+// 	private WebVisit previousNode;
+// 	private WebVisit nextNode;
+// 	private Date timestamp;
+
+// 	/**
+// 	  *  Constructor for a web visit. Initialize the three instance variables.
+// 	  *  @param u The URL of this visit
+// 	  *  @param pn The WebVisit previous to this one
+// 	  */
+// 	public WebVisit(String u, WebVisit pn, WebVisit nn) {
+// 		url = u;
+// 		previousNode = pn;
+// 		nextNode = nn;
+// 		timestamp = new Date();
+// 	}
+
+// 	/**
+// 	  * Return the previous WebVisit
+// 	  * @return the previous WebVisit object
+// 	  */
+// 	public WebVisit getPreviousNode() {
+// 		return previousNode;
+// 	}
+
+// 	public WebVisit getNextNode() {
+// 		return nextNode;
+// 	}
+
+// 	public void setNextNode(WebVisit next) {
+// 		nextNode = next;
+// 	}
+
+// 	/**
+// 	  * Return a neatly formatted String to represent this object so that it can be printed out for the user
+// 	  */
+// 	public String toString() {
+// 		return "Web visit: "+url+" (at "+timestamp+")";
+// 	}
+// }
+
 import java.util.Date;
 
 public class WebVisit {
-	private String url;
-	private WebVisit previousNode;
-	private WebVisit nextNode;
-	private Date timestamp;
+    private String url;
+    private WebVisit previousNode;
+    private WebVisit nextNode;
+    private Date timestamp;
 
-	/**
-	  *  Constructor for a web visit. Initialize the three instance variables.
-	  *  @param u The URL of this visit
-	  *  @param pn The WebVisit previous to this one
-	  */
-	public WebVisit(String u, WebVisit pn, WebVisit nn) {
-		url = u;
-		previousNode = pn;
-		nextNode = nn;
-		timestamp = new Date();
-	}
+    public WebVisit(String u, WebVisit pn, WebVisit nn) {
+        url = u;
+        previousNode = pn;
+        nextNode = nn;
+        timestamp = new Date();
+    }
 
-	/**
-	  * Return the previous WebVisit
-	  * @return the previous WebVisit object
-	  */
-	public WebVisit getPreviousNode() {
-		return previousNode;
-	}
+    public WebVisit getPreviousNode() {
+        return previousNode;
+    }
 
-	public WebVisit getNextNode() {
-		return nextNode;
-	}
-	
-	public void setNextNode(WebVisit next) {
-		nextNode = next;
-	}
+    public WebVisit getNextNode() {
+        return nextNode;
+    }
 
-	/**
-	  * Return a neatly formatted String to represent this object so that it can be printed out for the user
-	  */
-	public String toString() {
-		return "Web visit: "+url+" (at "+timestamp+")";
-	}
+    public void setNextNode(WebVisit next) {
+        nextNode = next;
+    }
+
+    public String toString() {
+        return "Web visit: " + url + " (at " + timestamp + ")";
+    }
 }
--- Browser History/browser.java
+++ Browser History/browser.java
@@ -1,36 +1,160 @@
-package Browser History;
- import java.io.*;
+// package Browser History;
+//  import java.io.*;
 
-public class browser {
-   
+// public class browser {
 
+// 	private boolean browsing;
+// 	private WebVisit currentVisit;
 
+// 	/**
+// 	  * Main method for the application. Create a new Browser object and set it to run.
+// 	  */
+// 	public static void main(String[] args) {
+// 		Browser b = new Browser();
+// 		b.run();
+// 	}
+
+// 	/**
+// 	  * Constructor for the Browser. Welcome the user and initialize the instance variables.
+// 	  */
+// 	public Browser() {
+// 		browsing = true;
+// 		currentVisit = null;
+// 		System.out.println("Welcome to FCS Browser v0.01");
+// 	}
+
+// 	/**
+// 	  * The primary method for the web browser: while the user is browsing the web, continually prompt, read, and execute commands.
+// 	  */
+// 	public void run() {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		while (browsing) {
+// 			try {
+// 				promptUser();
+// 				String cmd = br.readLine();
+// 				interpretAndExecute(cmd);
+// 			} catch (IOException e) {
+// 				System.out.println("Oops! There was an error reading your command.");
+// 			}
+// 		}
+// 	}
+
+// 	/**
+// 	  * Prompt the user with a message to the command line.
+// 	  */
+// 	private void promptUser() {
+// 		System.out.println("\nBrowser ready.\n(Commands: visit *web url*; history; back; quit; forward ;)");
+// 	}
+
+// 	/**
+// 	  * Given a string, determine which command (if any) is being issued and execute it
+// 	  *  hint: splitting at " " will be useful
+// 	  *  hint: notice that comparing Strings requires use of the .equals() method. Let's talk about it.
+// 	  * @param cmd The complete string entered by the user
+// 	  */
+// 	private void interpretAndExecute(String cmd) {
+// 		String[] commands = cmd.split(" ");
+// 		if (cmd.equals("back")) {
+// 			goBack();
+// 		} else if (cmd.equals("history")) {
+// 			viewHistory();
+// 		} else if (commands[0].equals("visit")) {
+// 			visitPage(commands[1]);
+// 		} else if (cmd.equals("quit")) {
+// 			quit();
+// 		} else if (cmd.equals("forward")){
+// 			goForward();
+
+// 		} else {
+// 			System.out.println("Command not recognized");
+// 		}
+// 	}
+
+// 	/**
+// 	  * Create a new WebVisit object according to the URL passed in, move it to the top of the stack.
+// 	  * @param url The URL of the new webpage the user would like to visit
+// 	  */
+// 	private void visitPage(String url) {
+// 		System.out.println("Now going to visit "+url);
+// 		WebVisit wv = new WebVisit(url, currentVisit, null);
+// 		if (currentVisit != null) {
+// 			currentVisit.setNextNode(wv);
+// 		}
+// 		currentVisit = wv;
+// 		System.out.println(currentVisit);
+// 	}
+// 	/**
+// 	  * If there is a previous visit in the browser's history, pop the current one off the top of the stack
+// 	  *  and print out the browser's new page (if it exists). If there is no page to go back to, alert the user and do nothing.
+// 	  */
+// 	private void goBack() {
+// 		if (currentVisit != null) {
+// 			System.out.println("Going back...");
+// 			currentVisit = currentVisit.getPreviousNode();
+// 			if (currentVisit != null) {
+// 				System.out.println(currentVisit);
+// 			}
+// 		} else {
+// 			System.out.println("No web visits in browser history");
+// 		}
+// 	}
+
+// 	private void goForward() {
+// 		if (currentVisit != null) {
+// 			System.out.print("Going forward...");
+// 			currentVisit = currentVisit.getNextNode();
+// 			if (currentVisit != null) {
+// 				System.out.println(currentVisit);
+// 			} else {
+// 				System.out.println("No next page to show.");
+// 			}
+// 		}
+// 	}
+
+// 	/**
+// 	  * View a list of all WebVisits in the browser's history; list the total number of visits.
+// 	  */
+// 	private void viewHistory() {
+// 		System.out.println("Showing browser history...");
+// 		WebVisit tmp = currentVisit;
+// 		int counter = 0;
+// 		while (tmp != null) {
+// 			System.out.println("\t~"+tmp);
+// 			tmp = tmp.getPreviousNode();
+// 			counter++;
+// 		}
+// 		System.out.println("\t\t"+counter+" total");
+// 	}
+// 	/**
+// 	  * Say goodbye; quit the program.
+// 	  */
+// 	private void quit() {
+// 		System.out.println("Quitting now...");
+// 		browsing = false;
+// 	}
+// }
+
+import java.io.*;
+
+public class Browser {
 
 	private boolean browsing;
 	private WebVisit currentVisit;
 
-	/**
-	  * Main method for the application. Create a new Browser object and set it to run.
-	  */
 	public static void main(String[] args) {
 		Browser b = new Browser();
 		b.run();
 	}
 
-	/**
-	  * Constructor for the Browser. Welcome the user and initialize the instance variables.
-	  */
 	public Browser() {
 		browsing = true;
 		currentVisit = null;
 		System.out.println("Welcome to FCS Browser v0.01");
 	}
 
-	/**
-	  * The primary method for the web browser: while the user is browsing the web, continually prompt, read, and execute commands.
-	  */
 	public void run() {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		while (browsing) {
 			try {
 				promptUser();
@@ -42,95 +166,81 @@ public class browser {
 		}
 	}
 
-	/**
-	  * Prompt the user with a message to the command line.
-	  */
 	private void promptUser() {
-		System.out.println("\nBrowser ready.\n(Commands: visit *web url*; history; back; quit; forward ;)");
+		System.out.println("\nBrowser ready.");
+		System.out.println("Commands: visit <url>, history, back, forward, quit");
 	}
 
-	/**
-	  * Given a string, determine which command (if any) is being issued and execute it
-	  *  hint: splitting at " " will be useful
-	  *  hint: notice that comparing Strings requires use of the .equals() method. Let's talk about it.
-	  * @param cmd The complete string entered by the user
-	  */
 	private void interpretAndExecute(String cmd) {
 		String[] commands = cmd.split(" ");
+
 		if (cmd.equals("back")) {
 			goBack();
 		} else if (cmd.equals("history")) {
 			viewHistory();
 		} else if (commands[0].equals("visit")) {
-			visitPage(commands[1]);
+			if (commands.length > 1) {
+				visitPage(commands[1]);
+			} else {
+				System.out.println("Please enter URL: visit google.com");
+			}
 		} else if (cmd.equals("quit")) {
 			quit();
-		} else if (cmd.equals("forward")){
+		} else if (cmd.equals("forward")) {
 			goForward();
-
 		} else {
 			System.out.println("Command not recognized");
 		}
 	}
 
-	/**
-	  * Create a new WebVisit object according to the URL passed in, move it to the top of the stack.
-	  * @param url The URL of the new webpage the user would like to visit
-	  */
 	private void visitPage(String url) {
-		System.out.println("Now going to visit "+url);
+		System.out.println("Now going to visit " + url);
+
 		WebVisit wv = new WebVisit(url, currentVisit, null);
+
 		if (currentVisit != null) {
 			currentVisit.setNextNode(wv);
 		}
+
 		currentVisit = wv;
 		System.out.println(currentVisit);
 	}
-	/**
-	  * If there is a previous visit in the browser's history, pop the current one off the top of the stack
-	  *  and print out the browser's new page (if it exists). If there is no page to go back to, alert the user and do nothing.
-	  */
+
 	private void goBack() {
-		if (currentVisit != null) {
+		if (currentVisit != null && currentVisit.getPreviousNode() != null) {
 			System.out.println("Going back...");
 			currentVisit = currentVisit.getPreviousNode();
-			if (currentVisit != null) {
-				System.out.println(currentVisit);
-			}
+			System.out.println(currentVisit);
 		} else {
-			System.out.println("No web visits in browser history");
+			System.out.println("No previous page.");
 		}
 	}
 
 	private void goForward() {
-		if (currentVisit != null) {
-			System.out.print("Going forward...");
+		if (currentVisit != null && currentVisit.getNextNode() != null) {
+			System.out.println("Going forward...");
 			currentVisit = currentVisit.getNextNode();
-			if (currentVisit != null) {
-				System.out.println(currentVisit);
-			} else {
-				System.out.println("No next page to show.");
-			}
+			System.out.println(currentVisit);
+		} else {
+			System.out.println("No next page to show.");
 		}
 	}
 
-	/**
-	  * View a list of all WebVisits in the browser's history; list the total number of visits.
-	  */
 	private void viewHistory() {
 		System.out.println("Showing browser history...");
+
 		WebVisit tmp = currentVisit;
 		int counter = 0;
+
 		while (tmp != null) {
-			System.out.println("\t~"+tmp);
+			System.out.println("\t~ " + tmp);
 			tmp = tmp.getPreviousNode();
 			counter++;
 		}
-		System.out.println("\t\t"+counter+" total");
+
+		System.out.println("\t\t" + counter + " total");
 	}
-	/**
-	  * Say goodbye; quit the program.
-	  */
+
 	private void quit() {
 		System.out.println("Quitting now...");
 		browsing = false;
